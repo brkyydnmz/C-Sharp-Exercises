502777558c30cdef27ed70ad10aaa210ff0c23fc
Berkay Dönmez <65166198+brkyydnmz@users.noreply.github.com>
2020-12-13 15:58:15 +0300

    Add files via upload

---
+++ Uygulama 6/Uygulama 6/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = maskedTextBox1.Text;  //label 3 ve 4 telefon tarih maskedTextBox larının altında 
+            label4.Text = maskedTextBox2.Text;
+        }
+    }
+}
