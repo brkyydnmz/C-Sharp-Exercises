91b43394e93d990d868949d143ae9a3a906bde66
Berkay Dönmez <65166198+brkyydnmz@users.noreply.github.com>
2021-01-21 18:15:07 +0300

    Chapter-7 Exercises 1-2 added

---
+++ Bölüm-7/Uygulama 1/Uygulama 1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] renkler = new string[5];  //5 elemanlı string olan bir dizi oluşturduk
+            renkler[0] = "Sarı";
+            renkler[1] = "Mavi";
+            renkler[2] = "Beyaz";
+            renkler[3] = "Turuncu";
+            renkler[4] = "Kırmızı";
+
+            label1.Text = renkler[2];
+        }
+    }
+}
---
+++ Bölüm-7/Uygulama 2/Uygulama 2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] iller = new string[4];
+            iller[0] = "Ankara";
+            iller[1] = "Balıkesir";
+            iller[2] = "İstanbul";
+            iller[3] = "İzmir";
+            label1.Text = iller[1];
+        }
+    }
+}
