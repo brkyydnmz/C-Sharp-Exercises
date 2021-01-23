3e43ea578cef35c91458041321014cec47de338b
Berkay Dönmez <65166198+brkyydnmz@users.noreply.github.com>
2021-01-24 01:07:27 +0300

    exercises 10 added

---
+++ Bölüm-7/Uygulama 10/Uygulama 10/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] dizi = { 5, 48, 36, 12, 55, 3, 14, 96, 71, 9 };
+
+            int enKucukSayı = dizi[0];
+
+            for(int i = 0; i < dizi.Length; i++)
+            {
+                if (dizi[i] < enKucukSayı)
+                {
+                    enKucukSayı = dizi[i];
+                }
+            }
+            label2.Text = enKucukSayı.ToString();
+        }
+    }
+}
