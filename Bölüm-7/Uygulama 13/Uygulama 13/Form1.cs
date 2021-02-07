ef1b65467be01e0b762a3d17e5f2ec600e02b933
Berkay Dönmez <65166198+brkyydnmz@users.noreply.github.com>
2021-02-07 23:28:58 +0300

    13 added

---
+++ Bölüm-7/Uygulama 13/Uygulama 13/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int sıra;
+        string[] isim = { "Ali", "Berkay", "Hasan", "Akif", "Kutay", "Emre" };
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for(int i=0; i < isim.Length; i++)
+            {
+                listBox1.Items.Add(isim[i]);
+            }    
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            sıra = Array.IndexOf(isim,"Berkay");   //Berkay (arrayden) index no sorgulama
+            label1.Text = "Berkay index sırası: "+sıra.ToString();
+        }
+    }
+}
