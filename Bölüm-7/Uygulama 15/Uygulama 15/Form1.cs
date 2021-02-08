030ab7f0766ef600034391ff1234503f9410cdae
Berkay Dönmez <65166198+brkyydnmz@users.noreply.github.com>
2021-02-09 01:07:31 +0300

    15 added

---
+++ Bölüm-7/Uygulama 15/Uygulama 15/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama_15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[,] dizi = new int[2, 2];  //matrix dizi için [,] kullanırız
+            dizi[0, 0] = 25;
+            dizi[0, 1] = 36;
+            dizi[1, 0] = 85;
+            dizi[1, 1] = 74;
+
+            label1.Text = dizi[1, 0].ToString(); //85 i verir
+        }
+    }
+}
