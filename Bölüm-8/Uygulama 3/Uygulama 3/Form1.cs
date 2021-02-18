27882aa29a29c3c73a8dbbcf111aea97328c760a
Berkay Dönmez <65166198+brkyydnmz@users.noreply.github.com>
2021-02-19 00:35:15 +0300

    3 added

---
+++ Bölüm-8/Uygulama 3/Uygulama 3/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Hesapla()
+        {
+            int sayi1 = Convert.ToInt32(textBox1.Text);
+            int sayi2 = Convert.ToInt32(textBox2.Text);
+            int sonuc = sayi1 + sayi2;
+            label1.Text = sonuc.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hesapla();
+        }
+    }
+}
