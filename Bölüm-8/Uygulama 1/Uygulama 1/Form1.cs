00a8be7ecfdfa30364b9a0f64776e6b9a77cb63a
Berkay Dönmez <65166198+brkyydnmz@users.noreply.github.com>
2021-02-19 00:34:46 +0300

    1 added

---
+++ Bölüm-8/Uygulama 1/Uygulama 1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void Yaz()  //Geriye değer döndürmeyen metottur 
+        {
+            label1.Text = "Merhaba";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Yaz(); //metodumuzu çağırarak burada kullandık.
+        }
+    }
+}
