52be534314af58bb43cb2def83103cda28149125
Berkay Dönmez <65166198+brkyydnmz@users.noreply.github.com>
2021-02-19 21:42:02 +0300

    9 10 added

---
+++ Bölüm-8/Uygulama 10/Uygulama 10/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int Faktoriyel(int a)
+        {
+            if (a == 0)
+                return 1;  // 0 faktoriyel 1 e eşit olduğu için 1 döndürüyor 
+            return a * Faktoriyel(a - 1);   // a=0 a ulaşana kadar azalarak çarpma yapar 1 er 1 er azalır    //bu durumda yinelenen metod oluyor buna rekürsif metod denir
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = Faktoriyel(Convert.ToInt32(textBox1.Text)).ToString();
+        }
+    }
+}
---
+++ Bölüm-8/Uygulama 9/Uygulama 9/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulama_9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int Hesapla(int sayi)  // heryerde kullanılabilir metod publictir //sayi değerini nerde alacağını altta belirttik
+        {
+            int sonuc = sayi * sayi;
+            return sonuc;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = Hesapla(Convert.ToInt32(textBox1.Text)).ToString();
+        }
+    }
+}
